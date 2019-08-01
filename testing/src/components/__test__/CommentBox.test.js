fbeda586a5330b6cf9026d4c7aafc294934d993e
Mohd Sahil <mohdsahil@Mohds-MacBook.local>
2019-08-01 07:46:45 +0530

    CommentBox Form

---
+++ testing/src/components/__test__/CommentBox.test.js
@@ -0,0 +1,7 @@
+import React from "react";
+import CommentBox from "../../components/CommentBox";
+import CommentBox from "../CommentBox";
+import { isMainThread } from "worker_threads";
+
+// Testing to make sure we have textarea inside of our component
+it("has a text area and a button", () => {});
